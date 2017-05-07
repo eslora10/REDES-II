b5cc3ebf7ecedc05bc8fe6bc98dce48304e99639
Unknown <santiagmario@gmail.com>
2017-05-07 01:56:44 -0700

    ping-pong
    Implementado protocolo ping-pong

--- SSL/src/G-2301-04-P1-server.c
+++ SSL/src/G-2301-04-P1-server.c
@@ -115,9 +115,8 @@ int main(int argc, char** argv) {
     int i, cl_sck;
     int long_index;
     char opt;
-    pthread_t thread, ssl_thread;
+    pthread_t thread, ssl_thread, pingthread;
     Sck_SSL *argAttend = NULL;
-
 
     for (i = 0; i < 37; i++)
         Commands[i] = FuncDefault;
@@ -136,6 +135,7 @@ int main(int argc, char** argv) {
     Commands[30] = whoCommand;
     Commands[31] = whoisCommand;
     Commands[34] = pingCommand;
+    Commands[35] = pongCommand;
     Commands[37] = awayCommand;
 
     static struct option options[] = {
@@ -199,6 +199,15 @@ int main(int argc, char** argv) {
         syslog(LOG_ERR, "Error bind");
         return -1;
     }
+	/*Hilo pingpong*/
+	if (pthread_create(&pingthread, NULL, pingpong, NULL) < 0) {
+            syslog(LOG_ERR, "Error creating thread");
+            return -1;
+        }
+	if (pthread_detach(pingthread) < 0) {
+            syslog(LOG_ERR, "Error  detach");
+            return -1;
+        }
 
     while (1) {
         if ((cl_sck = acceptSocket(sck)) < 0) {
